d06f48e6562d3712d893e79bc0481d17c9dd4aeb
nab138 <nab@nabdev.me>
2025-05-24 08:58:31 -0400

    Update authview

--- src/AuthView.swift
+++ src/AuthView.swift
@@ -4,9 +4,19 @@ struct AuthView: View {
   @State var email = ""
   @State var password = ""
   @State var isLoading = false
+  @State var signingUp = false
   @State var result: Result<Void, Error>?
 
   var body: some View {
+    VStack {
+      Text("Veo - Sign " + (signingUp ? "up" : "in"))
+        .font(.largeTitle)
+        .padding(.top, 20)
+      Text("Best list app ever")
+        .font(.subheadline)
+        .foregroundStyle(.secondary)
+        .padding(.bottom, 20)
+    }
     Form {
       Section {
         TextField("Email", text: $email)
@@ -29,11 +39,24 @@ struct AuthView: View {
         }
       }
 
+      VStack {
+        Spacer()
+        Text("Don't have an account? Sign up")
+          .font(.footnote)
+          .foregroundStyle(.gray)
+          .underline()
+          .padding(.bottom, 16)
+          .onTapGesture {
+            signingUp.toggle()
+          }
+      }
+
       if let result {
         Section {
           switch result {
           case .success:
-            Text("Signed in!")
+            Text("Signed " + (signingUp ? "up" : "in") + " successfully")
+              .foregroundStyle(.green)
           case .failure(let error):
             Text(error.localizedDescription).foregroundStyle(.red)
           }
@@ -48,10 +71,17 @@ struct AuthView: View {
       defer { isLoading = false }
 
       do {
-        try await supabase.auth.signIn(
+        if signingUp {
+          try await supabase.auth.signUp(
             email: email,
             password: password
-        )
+          )
+        } else {
+          try await supabase.auth.signIn(
+              email: email,
+              password: password
+          )
+        }
         result = .success(())
       } catch {
         result = .failure(error)
