7b73021ac812d08786eb6ffbe1528e598cb54338
nab138 <nab@nabdev.me>
2025-05-28 09:13:54 -0400

    Fix scrolling?

--- src/VeoListView.swift
+++ src/VeoListView.swift
@@ -150,7 +150,7 @@ struct VeoListView: View {
                                     }
                                 }
                                 .simultaneousGesture(
-                                    DragGesture(minimumDistance: 0)
+                                    DragGesture(minimumDistance: 8)
                                         .onChanged { value in
                                             // Only allow swipe if not pinching and no other swipe is active or it's the same index
                                             if isEditable(idx: idx) && pinchScale == 1 && (swipeIndex == nil || swipeIndex == idx) {
